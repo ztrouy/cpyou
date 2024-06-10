3ed2780a9d7ee97d161e6245ed067b4e8776005d
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-06-10 14:38:40 -0500

    Created file, and wrote rendering and logic for Builds List view

---
+++ client/src/components/builds/BuildsList.jsx
@@ -0,0 +1,39 @@
+import { Badge, Box, Chip, CircularProgress, Container, Paper, Typography } from "@mui/material"
+import { useEffect, useState } from "react"
+import { getBuilds } from "../../managers/buildManager.js"
+
+export const BuildsList = ({ loggedInUser }) => {
+    const [builds, setBuilds] = useState(null)
+    
+    useEffect(() => {
+        getBuilds().then(setBuilds)
+    }, [])
+
+    if (!builds) {
+        return <CircularProgress/>
+    }
+
+    return (
+        <Container sx={{display: "flex", justifyContent: "center"}}>
+            <Box sx={{width: 1, maxWidth: "800px"}}>
+                <Typography variant="h3" sx={{my: 2}}>Builds</Typography>
+                <Box sx={{display: "flex", flexDirection: "column", gap: 1}}>
+                    {builds.map(b => (
+                        <Paper sx={{p: 2}} elevation={3} key={`b-${b.id}`}>
+                            <Box sx={{display: "flex", gap: 1}}>
+                                <Typography variant="h5">{b.name}</Typography>
+                                <Chip 
+                                    label={`$${b.totalPrice}`}
+                                />
+                            </Box>
+                            <Typography>{b.userProfile.userName}</Typography>
+                            <Typography sx={{fontStyle: "italic"}}>{`${b.componentsQuantity} components`}</Typography>
+                        </Paper>
+                    ))}
+                </Box>
+            </Box>
+        </Container>
+    )
+}
+
+export default BuildsList
