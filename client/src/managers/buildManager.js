6199737de3685f47cb8a293a7dc2b1123f73c30c
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-06-10 15:45:46 -0500

    Added fetch for getting a Build by Id

--- client/src/managers/buildManager.js
+++ client/src/managers/buildManager.js
@@ -3,3 +3,7 @@ const _apiUrl = "/api/build"
 export const getBuilds = () => {
     return fetch(_apiUrl).then(res => res.json())
 }
+
+export const getSingleBuild = (buildId) => {
+    return fetch(`${_apiUrl}/${buildId}`).then(res => res.json())
+}
