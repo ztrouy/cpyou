7bd24271c9c9c44a1d543a470235eb3ee2ca215f
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-06-10 15:39:03 -0500

    Created initial DTO for Build Details

---
+++ Models/DTOs/Builds/BuildForBuildDetailsDTO.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CPYou.Models.DTOs;
+
+public class BuildForBuildDetailsDTO
+{
+    public int Id { get; set; }
+
+    [Required]
+    public int UserProfileId { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public string Content { get; set; }
+
+    [Required]
+    public DateTime DateCreated { get; set; }
+
+    // Calculated Properties
+    public decimal TotalPrice => Components.Sum(c => c.Price * c.Quantity);
+
+    public int ComponentsQuantity => Components.Sum(c => c.Quantity);
+    
+    public string FormattedDateCreated => DateCreated.ToString("MMMM dd, yyyy");
+
+    // Navigation Properties
+    public UserProfileForBuildDTO UserProfile { get; set; }
+
+    public List<ComponentForBuildDTO> Components { get; set; }
+}
