e276f00a92fb23a413e674768a2b0f9c858dd6f5
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-06-12 12:08:26 -0500

    Created file, and wrote initial DTO class properties

---
+++ Models/DTOs/Comments/CommentDTO.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CPYou.Models.DTOs;
+
+public class CommentDTO
+{
+    public int Id { get; set; }
+
+    [Required]
+    public int UserProfileId { get; set; }
+
+    [Required]
+    public int BuildId { get; set; }
+
+    [Required]
+    public string Content { get; set; }
+
+    [Required]
+    public DateTime DateCreated { get; set; }
+
+    // Navigation Properties
+    public UserProfileDTO UserProfile { get; set; }
+
+    public BuildDTO Build { get; set; }
+}
