a086fd92976f687fe64cd9d3769a9ff3306b36ed
Zachary Trouy <ztrouy@gmail.com>
2024-06-16 08:37:09 -0400

    Created file, and wrote rendering and logic for the Create Comment/Reply Form modal

---
+++ client/src/components/modals/CommentModal.jsx
@@ -0,0 +1,34 @@
+import { Box, Button, Dialog, DialogActions, DialogTitle, TextField } from "@mui/material"
+import { useState } from "react"
+
+export const CommentModal = ({ isOpen, toggle, submit, typeName}) => {
+    const [input, setInput] = useState("")
+    
+    const handleSubmit = () => {
+        submit(input)
+        setInput("")
+    }
+
+    return (
+        <Dialog open={isOpen} onClose={toggle} fullWidth>
+            <DialogTitle>Create {typeName}</DialogTitle>
+            <Box sx={{p: 2}}>
+                <TextField 
+                    type="text"
+                    placeholder="Write comment here..."
+                    autoFocus
+                    value={input}
+                    onChange={e => setInput(e.target.value)}
+                    fullWidth
+                />
+
+            </Box>
+            <DialogActions>
+                <Button onClick={toggle}>Cancel</Button>
+                <Button onClick={handleSubmit}>Submit</Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
+
+export default CommentModal
