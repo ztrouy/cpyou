5992842f1f308408c287513a8b05d27ab495eb2d
Zachary Trouy <ztrouy@gmail.com>
2024-06-19 13:25:30 -0500

    Made navbar sticky instead of static

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -11,32 +11,30 @@ export const NavBar = () => {
     }
     
     return (
-        <Box sx={{flexGrow: 1}}>
-            <AppBar position="static">
-                <Toolbar disableGutters sx={{justifyContent: "flex-end", pr: 1}}>
-                    <Box flexGrow={1}>
-                        <Button sx={{color: "white", ml: 1}} href="/">CP&You</Button>
-                    </Box>
-                    {loggedInUser ? (
-                        <>
-                            <Button variant="text" href="/builds" sx={{color: "white"}}>
-                                Builds
-                            </Button>
-                            <Button variant="text" href="/builds/new" sx={{color: "white"}}>
-                                New Build
-                            </Button>
-                            <Button onClick={e => handleLogout(e)} variant="text" href="/login" sx={{color: "white"}}>
-                                Logout
-                            </Button>
-                        </>
-                    ) : (
-                        <Button variant="text" href="/login" sx={{color: "white"}}>
-                            Login
+        <AppBar position="sticky">
+            <Toolbar disableGutters sx={{justifyContent: "flex-end", pr: 1}}>
+                <Box flexGrow={1}>
+                    <Button sx={{color: "white", ml: 1}} href="/">CP&You</Button>
+                </Box>
+                {loggedInUser ? (
+                    <>
+                        <Button variant="text" href="/builds" sx={{color: "white"}}>
+                            Builds
                         </Button>
-                    )}
-                </Toolbar>
-            </AppBar>
-        </Box>
+                        <Button variant="text" href="/builds/new" sx={{color: "white"}}>
+                            New Build
+                        </Button>
+                        <Button onClick={e => handleLogout(e)} variant="text" href="/login" sx={{color: "white"}}>
+                            Logout
+                        </Button>
+                    </>
+                ) : (
+                    <Button variant="text" href="/login" sx={{color: "white"}}>
+                        Login
+                    </Button>
+                )}
+            </Toolbar>
+        </AppBar>
     )
 }
 
