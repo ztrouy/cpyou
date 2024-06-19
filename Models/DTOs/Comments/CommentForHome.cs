e46c4342c7e649ae15f271a58eb6972c9df2ee0b
Zachary Trouy <ztrouy@gmail.com>
2024-06-19 13:22:30 -0500

    Created initial DTO for Comments for the Home Page

---
+++ Models/DTOs/Comments/CommentForHome.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CPYou.Models.DTOs;
+
+public class CommentForHomeDTO
+{
+    public int Id { get; set; }
+
+    [Required]
+    public int UserProfileId { get; set; }
+
+    [Required]
+    public int BuildId { get; set; }
+
+    [Required]
+    public string BuildName { get; set; }
+
+    [Required]
+    public string Content { get; set; }
+
+    [Required]
+    public DateTime DateCreated { get; set; }
+
+    // Calculated Properties
+    public string FormattedDateCreated => DateCreated.ToString("MMM dd, yyyy");
+
+    // Navigation Properties
+    public UserProfileForCommentDTO UserProfile { get; set; }
+
+    public List<ReplyForCommentDTO> Replies { get; set; }
+}
