a53fd7a0a148d48368b09cb824874c9797045801
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-06-20 12:52:35 -0500

    Created initial CPU DTOs to match functionality of generic Component DTOs

---
+++ Models/DTOs/Components/CPUs/CPUNoNavDTO.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CPYou.Models.DTOs;
+
+public class CPUNoNavDTO
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public decimal Price { get; set; }
+
+    [Required]
+    public int InterfaceId { get; set; }
+
+    [Required]
+    public int TDP { get; set; }
+}
