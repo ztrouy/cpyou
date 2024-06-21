0f9df710ef5eff5e34a926bea43e4d17b95eb2c7
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-06-20 22:52:00 -0500

    Added properties for Id and UserProfileId

--- Models/DTOs/Builds/BuildEditDTO.cs
+++ Models/DTOs/Builds/BuildEditDTO.cs
@@ -5,6 +5,12 @@ namespace CPYou.Models.DTOs;
 public class BuildEditDTO
 {
     [Required]
+    public int Id { get; set; }
+
+    [Required]
+    public int UserProfileId { get; set; }
+    
+    [Required]
     public string Name { get; set; }
 
     [Required]
